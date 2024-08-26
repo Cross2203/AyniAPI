79a95b0e991d4f566328067e3a71e988c7f51ab6
Cross2203 <ciae1996@gmail.com>
2024-08-26 12:43:36 -0500

    feat: improve s3 files management

--- records/get_file.py
+++ records/get_file.py
@@ -1,21 +1,25 @@
 import boto3
 from django.conf import settings
 
-# Configura las credenciales de AWS
 s3 = boto3.client(
     's3',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION
+    region_name=settings.AWS_S3_REGION_NAME
 )
- 
-print(s3)
 
-def get_s3_file_url(object_name, folder_name):
+def get_s3_file_url(object_name, folder_name, expiration=3600):
     object_key = f"{folder_name}/{object_name}"
-    url = s3.generate_presigned_url(
-        ClientMethod='get_object',
-        Params={'Bucket': settings.S3_BUCKET_NAME, 'Key': object_key},
-        ExpiresIn=3600  
-    )
-    return url
+    try:
+        url = s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                'Key': object_key
+            },
+            ExpiresIn=expiration
+        )
+        return url
+    except Exception as e:
+        print(f"Error al generar la URL prefirmada: {e}")
+        return None
--- records/upload_file.py
+++ records/upload_file.py
@@ -2,12 +2,14 @@ import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
 from uuid import uuid4
+import os
 
+# Configuración del cliente S3
 s3 = boto3.client(
     's3',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION
+    region_name=settings.AWS_S3_REGION_NAME
 )
 
 def upload_file_to_s3(file, folder_name):
@@ -17,19 +19,19 @@ def upload_file_to_s3(file, folder_name):
 
         s3.upload_fileobj(
             file,
-            settings.S3_BUCKET_NAME,
+            settings.AWS_STORAGE_BUCKET_NAME,
             object_key,
-            ExtraArgs={'ACL': 'public-read'}
+            ExtraArgs={'ACL': settings.AWS_DEFAULT_ACL}
         )
 
-        file_url = f"https://{settings.S3_BUCKET_NAME}.s3.{settings.AWS_S3_REGION_NAME}.amazonaws.com/{object_key}"
+        file_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{object_key}"
         return file_url
 
     except ClientError as e:
         error_code = e.response['Error']['Code']
         error_message = e.response['Error']['Message']
         print(f"Error al subir el archivo a S3: {error_code} - {error_message}")
-        raise e
+        raise
     except Exception as e:
         print(f"Error inesperado al subir el archivo a S3: {e}")
-        raise e
+        raise
